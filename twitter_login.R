310ac3e1c8c0ff9fe21064bc59c0f435bc0b7486
Zachary Kitt <spam.zachary@gmail.com>
2017-02-15 17:54:18 -0800

    note to remove sensitive info and added scraping intro

--- twitter_login.R
+++
@@ -1,7 +0,0 @@
-# Stores access tokens from dev.twitter.com
-api_key = "ThdyCndDxrLewFHvuYIV4Xb49"
-api_secret ="L9QPgXrB0H3pPdXJwzH7vU38o3wfMRXlSi8utvBWYKuHoHZWod"
-access_token = "235832591-DrDjV1M7poSxvBh9AWTCZHDWikKRFnuxtRyvBHnt"
-access_token_secret = "woMMg3aIqU8FAkPbkkMZpXSE1uUJYIWfOrN5aBQiE3cfA"
-# Authenticate
-setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
